cf4098235b082fc14189b006cb46a75bf545138d
jitesh@linkederp.com <jitesh@linkederp.com>
2025-08-20 21:50:56 +0530

    [Add] Timesheet Cross-Company Blocker test

--- TimesheetTest/__manifest__.py
+++ TimesheetTest/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'JV',
     'website': 'https://Linkederp.com',
     'license': 'AGPL-3',
-    'depends': ['account', 'project','timesheet'],
+    'depends': ['account', 'project'],
     'data': [],
     'installable': True,
     'application': True,
