408f62069f21fb7fd498e08c637aabb4b6984f01
masbin <m.bintangkhatulistiwa@gmail.com>
2025-12-05 21:56:36 +0700

    TimesheetTest Module

---
+++ timesheet_linked/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Timesheet CrossComapny Blocker',
+    'version': '1.0.0',
+    'summary': 'Project and User company should be same while filling Timesheet',
+    'category': 'Accounting',
+    'author': 'LinkedErp / JV',
+    'website': 'https://Linkederp.com',
+    'license': 'AGPL-3',
+    'depends': ['hr_timesheet'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ timesheet_linked/models/timesheet.py
@@ -0,0 +1,35 @@
+from odoo import models, api, _, fields
+from odoo.exceptions import ValidationError
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    @api.constrains('project_id')
+    def _check_project_company_vs_current_company(self):
+        """Prevent logging timesheet if project company != currently selected company."""
+        current_company = self.env.company  # company chosen in top-right profile switcher
+        for record in self:
+            if record.project_id and record.project_id.company_id != current_company:
+                self.project_id = False  # clear project selection
+                raise ValidationError(
+                    _("You cannot log a timesheet on a project that belongs to another company.\n"
+                      "Your current company: %s\nProject company: %s") %
+                    (current_company.display_name, record.project_id.company_id.display_name)
+                )
+                return {'warning': warning}
+
+    @api.onchange('project_id')
+    def _onchange_project_company_vs_current_company(self):
+        """Show warning immediately in form view when project != current company."""
+        current_company = self.env.company
+        if self.project_id and self.project_id.company_id != current_company:
+            self.project_id = False # clear project selection
+            return {
+                'warning': {
+                    'title': _("Invalid Project"),
+                    'message': _("This project belongs to another company. "
+                                 "You cannot log a timesheet for it while working in %s.")
+                               % current_company.display_name,
+                }
+            }
