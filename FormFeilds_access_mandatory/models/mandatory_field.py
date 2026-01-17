8e1b5489307c9daf3789bced57600e31a53b30b2
jit@linkederp <jit@linkederp>
2026-01-17 17:35:59 +0530

    [Test] Other Form Field Required testing .

--- FormFeilds_access_mandatory/models/mandatory_field.py
+++ FormFeilds_access_mandatory/models/mandatory_field.py
@@ -3,16 +3,21 @@ from odoo import models, fields
 class MandatoryField(models.Model):
     _name = "mandatory.field.rule"
     _description = "Mandatory Field Rule"
+    _rec_name = "field_id"
 
-    name = fields.Char(required=True)
     model_id = fields.Many2one(
         "ir.model",
+        string="Model",
         required=True,
-        domain=[("model", "=", "res.partner")]
+        ondelete="cascade"
     )
+
     field_id = fields.Many2one(
         "ir.model.fields",
+        string="Field",
         required=True,
+        ondelete="cascade",
         domain="[('model_id', '=', model_id), ('ttype', 'not in', ('one2many', 'many2many'))]"
     )
+
     active = fields.Boolean(default=True)
