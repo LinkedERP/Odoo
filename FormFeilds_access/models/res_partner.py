85af640a7e9c020675327e9acfd46163961da962
jit@linkederp <jit@linkederp>
2026-01-17 17:01:41 +0530

    [Test] Form Field Required testing.

--- FormFeilds_access/models/res_partner.py
+++ FormFeilds_access/models/res_partner.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
@@ -6,3 +7,21 @@ class ResPartner(models.Model):
     require_phone = fields.Boolean(string="Require Phone")
     require_email = fields.Boolean(string="Require Email")
     require_vat = fields.Boolean(string="Require VAT")
+
+    @api.constrains('require_phone', 'phone')
+    def _check_required_phone(self):
+        for rec in self:
+            if rec.require_phone and not rec.phone:
+                raise ValidationError("Phone is required as per configuration.")
+
+    @api.constrains('require_email', 'email')
+    def _check_required_email(self):
+        for rec in self:
+            if rec.require_email and not rec.email:
+                raise ValidationError("Email is required as per configuration.")
+
+    @api.constrains('require_vat', 'vat')
+    def _check_required_vat(self):
+        for rec in self:
+            if rec.require_vat and not rec.vat:
+                raise ValidationError("VAT is required as per configuration.")
