e01781c7ebef076ad471826e427d4d0e46c12ca3
jit@linkederp <jit@linkederp>
2026-01-17 17:25:15 +0530

    [Test] Other Form Field Required testing .

---
+++ FormFeilds_access_mandatory/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Mandatory Field Contact Dynamic Required Fields",
+    "version": "1.0.0",
+    "category": "Contacts",
+    "summary": "Make contact fields required dynamically",
+    "depends": ["base", "contacts"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ FormFeilds_access_mandatory/models/mandatory_field.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+class MandatoryField(models.Model)
+    _name = mandatory.field.rule
+    _description = Mandatory Field Rule
+
+    name = fields.Char(required=True)
+    model_id = fields.Many2one(
+        ir.model,
+        required=True,
+        domain=[(model, =, res.partner)]
+    )
+    field_id = fields.Many2one(
+        ir.model.fields,
+        required=True,
+        domain=[('model_id', '=', model_id), ('ttype', 'not in', ('one2many', 'many2many'))]
+    )
+    active = fields.Boolean(default=True)
