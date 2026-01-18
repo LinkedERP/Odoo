d4b3f9384f3329b1a5548a7133ee59c02741cac2
masbin <m.bintangkhatulistiwa@gmail.com>
2026-01-18 23:17:56 +0700

    Bump version numbers in __manifest__.py for multiple modules

--- FormFeilds_access/__manifest__.py
+++ FormFeilds_access/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Contact Dynamic Required Fields",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "category": "Contacts",
     "summary": "Make contact fields required dynamically",
     "depends": ["base", "contacts"],
--- helpdesk_ticket_type/__manifest__.py
+++ helpdesk_ticket_type/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Helpdesk Task Type Analysis',
-    'version': '1.0',
+    'version': '1.0.1',
     'summary': 'Add Task Type (Service/Change Request) to Helpdesk Ticket and Report Analysis',
     'depends': ['helpdesk'],
     'data': [
