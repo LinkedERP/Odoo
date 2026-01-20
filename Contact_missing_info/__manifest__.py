02abaa60d81ddf3dbbaf13561d6b161a4a58f33b
jit@linkederp <jit@linkederp>
2026-01-20 16:47:21 +0530

    [Report] Conatact missing info.

---
+++ Contact_missing_info/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Contacts Missing Mandatory Info Report",
+    "version": "1.0",
+    "category": "Contacts",
+    "summary": "Report of contacts missing email, phone and company",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_report_views.xml",
+    ],
+    "installable": True,
+}
