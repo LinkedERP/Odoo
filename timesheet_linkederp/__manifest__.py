c91637562758c1ce463a9bd8c3970250e3fb8072
Mas Bintang <bintangbluzz@gmail.com>
2026-04-15 12:41:20 +0700

    [NEW] timesheet: enforce project company consistency in timesheet logging

---
+++ timesheet_linkederp/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Timesheet Cross Company Blocker',
+    'version': '1.0.2',
+    'summary': 'Project and User company should be same while filling Timesheet',
+    'category': 'Accounting',
+    'author': 'JV',
+    'website': 'https://Linkederp.com',
+    'license': 'AGPL-3',
+    'depends': ['account', 'hr_timesheet'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
