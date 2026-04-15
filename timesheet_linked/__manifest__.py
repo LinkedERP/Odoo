bbbd0ba34aa833a26a02d34e95d723df1ffb04bd
Mas Bintang <bintangbluzz@gmail.com>
2026-04-15 12:35:11 +0700

    [IMP] __manifest__: update module name, summary, and dependencies

--- timesheet_linked/__manifest__.py
+++ timesheet_linked/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'Timesheet Cross Company Blocker',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'summary': 'Ensure project and user company must match when filling timesheets',
     'category': 'Services/Timesheets',
     'author': 'LinkedERP / JV',
     'website': 'https://linkederp.com',
     'license': 'AGPL-3',
     'depends': [
+        'account',
         'hr_timesheet',
         'project',
     ],
