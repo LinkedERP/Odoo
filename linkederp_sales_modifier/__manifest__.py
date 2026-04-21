5de1ac1ff0e82049fbc2462fde2f7283d0c963c4
Mas Bintang <bintangbluzz@gmail.com>
2026-04-21 23:20:10 +0700

    [IMP] sale_order: allow locking SO regardless of invoice status

--- linkederp_sales_modifier/__manifest__.py
+++ linkederp_sales_modifier/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'LinkedERP Sales Modifier',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'summary': 'Hide locked Sales Orders from default views and timesheet SO selection',
     'category': 'Sales',
     'author': 'LinkedERP',
