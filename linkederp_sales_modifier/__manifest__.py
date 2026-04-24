9e226c0b0b7d71f5b3893f15096a3c704ebb8982
Mas Bintang <bintangbluzz@gmail.com>
2026-04-24 13:27:43 +0700

    [IMP] sale: hide locked sales orders in main and onboarding actions

--- linkederp_sales_modifier/__manifest__.py
+++ linkederp_sales_modifier/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'LinkedERP Sales Modifier',
-    'version': '1.0.2',
+    'version': '1.0.3',
     'summary': 'Hide locked Sales Orders from default views and timesheet SO selection',
     'category': 'Sales',
     'author': 'LinkedERP',
