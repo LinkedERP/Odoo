ab4f53dcbdcb573281302654b1c72af7508bce40
Mas Bintang <bintangbluzz@gmail.com>
2026-05-08 13:44:39 +0700

    [IMP] LinkedERP Sales Modifier: update version to 1.0.4 and adjust default filters for Sales Orders

--- linkederp_sales_modifier/__manifest__.py
+++ linkederp_sales_modifier/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'LinkedERP Sales Modifier',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'summary': 'Hide locked Sales Orders from default views and timesheet SO selection',
     'category': 'Sales',
     'author': 'LinkedERP',
