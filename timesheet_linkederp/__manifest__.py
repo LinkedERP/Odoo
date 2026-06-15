62b6e7a4711c9ab22c0962273ee907f0dd27d289
Mas Bintang <bintangbluzz@gmail.com>
2026-06-15 11:07:24 +0700

    [IMP] Filter for project in timesheet, if sale order is complete will not show in timesheet

--- timesheet_linkederp/__manifest__.py
+++ timesheet_linkederp/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'JV',
     'website': 'https://Linkederp.com',
     'license': 'AGPL-3',
-    'depends': ['account', 'hr_timesheet'],
+    'depends': ['account', 'hr_timesheet', 'sale_timesheet','helpdesk_sale_timesheet'],
     'data': [],
     'installable': True,
     'application': True,
