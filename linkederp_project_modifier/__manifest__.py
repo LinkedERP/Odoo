22b80f9f61d9971a5a306863324c88f16ca1681a
Mas Bintang <bintangbluzz@gmail.com>
2026-06-30 20:37:41 +0700

    [FIX] timesheet line readonly in project task and helpdesk task

--- linkederp_project_modifier/__manifest__.py
+++ linkederp_project_modifier/__manifest__.py
@@ -1,13 +1,13 @@
 {
     'name': 'LinkedERP Project Modifier',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'summary': 'Adjustment for project and task views, also will related with time sheet',
     'category': 'Project',
     'author': 'Muhammad Bintang',
     'website': 'https://linkederp.com',
     'license': 'LGPL-3',
     'depends': [
-        'project', 'sale_project',
+        'project', 'sale_project', 'hr_timesheet',
     ],
     'data': [
         'views/project_views_modifier.xml',
